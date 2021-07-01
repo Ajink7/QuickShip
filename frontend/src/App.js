41502af106d1a511150374f69da9654c129ca790
Ajinkya Kadam <ajinkadam@gmail.com>
2021-07-01 16:31:01 +0530

    User Register Reducer, Action, Screen and Logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 				<Container>
 					<Route exact path='/' component={HomeScreen} />
 					<Route path='/login' component={LoginScreen} />
+					<Route path='/register' component={RegisterScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
 					<Route path='/cart/:id?' component={CartScreen}></Route>
 				</Container>
