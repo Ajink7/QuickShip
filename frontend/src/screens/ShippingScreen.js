884d0b6c7a834fe374ab6ff83294fed17c914d1b
Ajinkya Kadam <ajinkadam@gmail.com>
2021-07-05 11:45:21 +0530

    Payment Screen and Checkout Steps section added

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import CheckoutSteps from '../components/CheckoutSteps'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions'
 const ShippingScreen = ({ history }) => {
@@ -20,6 +21,7 @@ const ShippingScreen = ({ history }) => {
 	}
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2></CheckoutSteps>
 			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId='address'>
