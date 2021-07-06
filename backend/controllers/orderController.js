058c130c77d03d0305dc67d65245f6afc84bd96c
Ajinkya Kadam <ajinkadam@gmail.com>
2021-07-07 01:59:06 +0530

    Order Pay reducer, action and backend endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,4 +46,24 @@ const getOrderById = asyncHandler(async (req, res) => {
 	}
 })
 
-export { addOrderItems, getOrderById }
+// GETo rder by id, private, /api/orders/:id/pay
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id)
+	if (order) {
+		order.isPaid = true
+		order.paidAt = Date.now()
+		order.paymentResult = {
+			id: req.body.id,
+			status: req.body.status,
+			update_time: req.body.update_time,
+			email_address: req.body.payer.email_address,
+		}
+		const updatedOrder = await order.save()
+		res.json(updatedOrder)
+	} else {
+		res.status(201)
+		throw new Error('Order not found')
+	}
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
