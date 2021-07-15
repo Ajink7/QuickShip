ff7a9d64c8fcdc1e0e4b4e361d7b0707d2525d57
Ajinkya Kadam <ajinkadam@gmail.com>
2021-07-15 18:31:28 +0530

    UserEditScreen and edit backend routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
 	updateUserProfile,
 	getUsers,
 	deleteUser,
+	getUserById,
+	updateUser,
 } from '../controllers/userController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
@@ -16,5 +18,9 @@ router
 	.get(protect, getUserProfile)
 	.put(protect, updateUserProfile)
 router.route('/').post(registerUser).get(protect, admin, getUsers)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+	.route('/:id')
+	.delete(protect, admin, deleteUser)
+	.get(protect, admin, getUserById)
+	.put(protect, admin, updateUser)
 export default router
